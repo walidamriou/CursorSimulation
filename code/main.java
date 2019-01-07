53bbc1ba0748144380ca1aeba665215459ae1d67
Walid Amriou <walidamriou@gmail.com>
2019-01-07 23:15:29 +0100

    Update main.java

--- code/main.java
+++ code/main.java
@@ -1,33 +1,29 @@
 import java.util.Scanner;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int lignes;
         int colones;
         String key;
 
         Scanner reader = new Scanner(System.in);
+
         curseur a = new curseur();
-        
+
         System.out.println("Nombre de lighnes? ");
         lignes = reader.nextInt();
+
         System.out.println("Nombre de colones? ");
         colones = reader.nextInt();
-        
+
         a.lignes(lignes);
         a.colones(colones);
-        
+
         while (true){
             key = reader.next();
-            if(key!=null)
-               a.position(key);
-            key=null;
+            a.position(key);
         }
-        
+
     }
 }
